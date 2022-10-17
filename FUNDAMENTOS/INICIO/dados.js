f883fd81c252778e69c5af47bdba1f7c676db617
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-10-17 15:58:45 -0300

    fundamentos

---
+++ FUNDAMENTOS/INICIO/dados.js
@@ -0,0 +1,15 @@
+var quantidade = 10
+var nome = "caneta"
+let variavel = 1.7
+let idade = 47
+let boleana = false
+
+console.log(typeof nome) // string
+console.log(typeof quantidade) // number
+console.log(typeof variavel) // number
+console.log(typeof boleana) // boolean
+
+const pi = 3.14
+// error -> pi = 2 
+
+console.log(pi)
---
+++ FUNDAMENTOS/INICIO/oláMundo.js
@@ -0,0 +1,15 @@
+// comentario 1 linha
+
+/* 
+*comentario muitas linhas
+*/
+
+console.log("Olá mundo JS")
+
+{
+    {
+        console.log('bloco de codigo')
+    }
+    
+}
+
